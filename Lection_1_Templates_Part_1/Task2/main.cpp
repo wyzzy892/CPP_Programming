67f1c56f6bd42fd2bb5c1ce73b9d1d45aa331736
Wyzzy <ilgizn395@gmail.com>
2022-10-02 21:28:52 +0300

    works

--- Lection_1_Templates_Part_1/Task2/main.cpp
+++ Lection_1_Templates_Part_1/Task2/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include "my_templ.hpp"
 
+int power(int value);
+
 int main()
 {
-	iter(arr, 10, foo);
+	size_t n = 10;
+	int* arr = new int[n];
+	for(size_t i=0; i<n; i++)
+		arr[i]=i+1;
+	
+	iter(arr, n, power);
+	
+	delete[] arr;
 	
 	return 0;
+}
+
+int power(int value) {
+	return value * value;
 }
--- Lection_1_Templates_Part_1/Task2/my_templ.hpp
+++ Lection_1_Templates_Part_1/Task2/my_templ.hpp
@@ -1,16 +1,10 @@
 #ifndef MY_TEMPL_HPP
 #define MY_TEMPL_HPP
 
-template<typename T1, typename T2, typename T3>
-void iter(T1* arr, T2 size, T3(*f)(T1)) {
-	for (T1* it = arr; it != (it + size); ++it)
-		std::cout<<f(*it);
+template<typename T1, typename T2>
+void iter(T1* arr, size_t size, T2(*f)(T1)) {
+	for (size_t i = 0; i < size; i++)
+		std::cout << f(arr[i])<<std::endl;
 }
-
-template<typename T>
-auto foo(T el) {
-	return T * T;
-}
-
 
 #endif
